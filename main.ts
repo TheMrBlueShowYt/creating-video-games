ecd11c24fc151601c9d1a98bf132be82a399568e
TheMrBlueShowYt <131374806+TheMrBlueShowYt@users.noreply.github.com>
2023-04-20 10:07:35 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (player1.overlapsWith(player2)) {
+        info.changeScoreBy(1)
+    }
+})
+info.onScore(10, function () {
+    game.setGameOverMessage(true, "YOU WIN!! :D")
+    game.setGameOverEffect(true, effects.clouds)
+    game.gameOver(true)
+})
+let player2: Sprite = null
+let player1: Sprite = null
+info.setScore(0)
+scene.setBackgroundImage(assets.image`background`)
+player1 = sprites.create(assets.image`player`, SpriteKind.Player)
+player1.setPosition(72, 91)
+player2 = sprites.create(assets.image`monke`, SpriteKind.Enemy)
+player2.setVelocity(100, 85)
+player2.setBounceOnWall(true)
