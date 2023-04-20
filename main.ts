09538ea7c93810736ceb6925d2aaa27f3c6a8cc7
TheMrBlueShowYt <131374806+TheMrBlueShowYt@users.noreply.github.com>
2023-04-20 10:15:57 -0400

    added lives

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (player1.overlapsWith(player2)) {
         info.changeScoreBy(1)
+    } else {
+        info.changeLifeBy(-1)
+        info.changeScoreBy(-1)
     }
 })
 info.onScore(10, function () {
-    game.setGameOverMessage(true, "YOU WIN!! :D")
-    game.setGameOverEffect(true, effects.clouds)
     game.gameOver(true)
 })
 let player2: Sprite = null
 let player1: Sprite = null
+info.setLife(3)
+game.setGameOverMessage(true, "YOU WIN!! :D")
+game.setGameOverEffect(true, effects.clouds)
 info.setScore(0)
 scene.setBackgroundImage(assets.image`background`)
 player1 = sprites.create(assets.image`player`, SpriteKind.Player)
